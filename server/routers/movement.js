1f8e0356ba4894242f39edebfebee72e67af61b8
Heather Ingargiola <h.ingargiola88@gmail.com>
2021-04-08 15:15:16 -0500

    corrected frenchBread to FrenvhBread

--- server/routers/movement.js
+++ server/routers/movement.js
@@ -1,37 +1,37 @@
 const express = require("express");
-const Movement = require("../models/movement");
+const FrenchBread = require("../models/movement").model;
 const router = express.Router();
 
 router.post("/", (request, response) => {
-  const newFrenchBread = new FrenchBread.model(request.body);
+  const newFrenchBread = FrenchBread.new(request.body);
   newFrenchBread.save((err, frenchBread) => {
     return err ? response.sendStatus(500).json(err) : response.json(frenchBread);
   });
 });
 
 router.post("/", (request, response) => {
-  const newFrenchBread = new frenchBread.model(request.body);
+  const newFrenchBread = FrenchBread.new(request.body);
   newFrenchBread.save((err, frenchBread) => {
     return err ? response.sendStatus(500).json(frenchBread) : response.json(err);
   });
 });
 
 router.get("/", (request, response) => {
-  frenchBread.model.find({}, (error, data) => {
+  FrenchBread.new.find({}, (error, data) => {
     if (error) return response.sendStatus(500).json(error);
     return response.json(data);
   });
 });
 
 router.get("/:id", (request, response) => {
-  frenchBread.model.findById(request.params.id, (error, data) => {
+  FrenchBread.new.findById(request.params.id, (error, data) => {
     if (error) return response.sendStatus(500).json(error);
     return response.json(data);
   });
 });
 
 router.delete('/:id', (request, response) => {
-  frenchBread.model.findByIdAndRemove(request.params.id, {}, (error, data) => {
+  FrenchBread.new.findByIdAndRemove(request.params.id, {}, (error, data) => {
     if (error) return response.sendStatus(500).json(error);
     return response.json(data)
   });
@@ -39,7 +39,7 @@ router.delete('/:id', (request, response) => {
 
 router.put("/:id", (request, response) => {
   const body = request.body;
-  frenchBread.model.findByIdAndUpdate(
+  FrenchBread.new.findByIdAndUpdate(
     request.params.id,
     {
       $set: {
